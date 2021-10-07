8b0cc8beac84b2512bff1370d7450b1dfb830137
Palle Raabjerg <github@raabjerg.mm.st>
2021-10-07 22:14:12 +0200

    Adjust extension to work with new Funimation player.

--- content_script.js
+++ content_script.js
@@ -1,17 +1,18 @@
 // Find <video> element for seeking functionality
-const videotags = document.getElementsByTagName('video');
-const player = videotags[0]
+// const videotags = document.getElementsByTagName('video');
+// const player = videotags[0]
 
 // Find info text element (upper part of UI)
-const vjsdocktext = document.querySelector('#brightcove-player > .vjs-dock-text');
+// const vjsdocktext = document.querySelector('#brightcove-player > .vjs-dock-text');
 // Find control bar element (lower part of UI)
-const vjscontrolbar = document.querySelector('#brightcove-player > .vjs-control-bar');
+// const vjscontrolbar = document.querySelector('.vjs-control-bar');
 // Keep track of UI hidden state
 let hidden = false;
 
 
 // Wind back 1 second if not at beginning
 function windBack() {
+  const player = document.getElementsByTagName('video')[0];
   if(player.currentTime > 1) {
     player.currentTime -= 1;
   }
@@ -19,6 +20,7 @@ function windBack() {
 
 // Wind forward 1 second if not at end
 function windForward() {
+  const player = document.getElementsByTagName('video')[0];
   if(player.currentTime < player.duration - 1) {
     player.currentTime += 1;
   }
@@ -29,25 +31,23 @@ function runContentScript() {
   document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     // On right arrow, wind 1 second forward
-    if (keyName === 'ArrowRight' && (!event.shiftKey)) {
+    if (keyName === 'PageDown' || keyName === 'ArrowDown') {
       windForward();
     }
     // On left arrow, wind 1 second back
-    else if (keyName === 'ArrowLeft' && (!event.shiftKey)) {
+    else if (keyName === 'PageUp' || keyName === 'ArrowUp') {
       windBack();
     }
     // On z, hide or show the UI
     else if (keyName === 'z') {
       if (hidden) {
-        document.querySelector('#funimation-gradient').classList.remove('disappeared');
-        vjsdocktext.classList.remove('disappeared');
-        vjscontrolbar.classList.remove('disappeared');
+        document.querySelector('.meta-overlay').classList.remove('disappeared');
+        document.querySelector('.vjs-control-bar').classList.remove('disappeared');
         hidden = false;
       }
       else {
-        document.querySelector('#funimation-gradient').classList.add('disappeared');
-        vjsdocktext.classList.add('disappeared');
-        vjscontrolbar.classList.add('disappeared');
+        document.querySelector('.meta-overlay').classList.add('disappeared');
+        document.querySelector('.vjs-control-bar').classList.add('disappeared');
         hidden = true;
       }
     }
